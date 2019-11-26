630304b9886bce48a0acb4f6327b03fbb0eded34
carloseduardobanjar <kadubanjar8@gmail.com>
2019-11-26 09:27:17 -0300

    fé

---
+++ Trabalho_GitHub_Rafael_Filipe_1B.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+
+/*
+	Conta todas as letras 'letra' contidas em 'texto'
+*/
+int contaLetras(char letra, string texto)
+{
+}
+
+/*
+	Substitui todas as ocorrencias de 'letra1' por 'letra2' contidas em 'texto'
+*/
+void substituirLetra(char letra1,char letra2, string texto)
+{
+}
+
+/*
+	Conta todas as ocorrencias de 'palavra' contidas em 'texto'
+*/
+int contaPalavras(string palavra, string texto)
+{
+}
+
+/*
+	Substitui todas as ocorrencias de 'palavra1' por 'palavra2' em 'texto'
+*/
+void substituirPalavras(string palavra1, string palavra2, string texto)
+{
+}
+
+int main(){
+
+	cout << "Entre com o texto para fazer o teste:";
+
+
+
+
+	return 0;
+}
